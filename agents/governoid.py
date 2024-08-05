c7968948f3716ed7d76ab1edbabc0eac33fd8e4e
Giulio Trichilo <giulio.trichilo.pay@gmail.com>
2024-08-05 22:44:49 +0200

    llm buyer and seller agent agree on a purchase price

--- agents/governoid.py
+++ agents/governoid.py
@@ -7,11 +7,23 @@ from typing import Any, Dict, List, Union
 from dotenv import load_dotenv
 import os
 
+from agents.lib.think import think_litellm
+
 load_dotenv()
 
 class Governoid:
-    def __init__(self, agent_id: int, provider_url: str, erc721_abi_path: str, commitment_abi_path: str):
+    def __init__(
+        self,
+        agent_id: int,
+        agent_role: str,
+        provider_url: str,
+        erc721_abi_path: str,
+        commitment_abi_path: str
+    ) -> None:
+        
         self.agent_id = agent_id
+        self.agent_role = agent_role
+        self.agent_name = agent_role + "_" + str(agent_id)
         
         self.web3 = Web3(Web3.HTTPProvider(provider_url))
         
@@ -30,6 +42,43 @@ class Governoid:
         
         self.erc721_contract: Union[None, Contract] = None
         self.commitment_contract: Union[None, Contract] = None
+
+        # LLM stuff
+        self.system_message = ""
+
+        if self.agent_role == "seller":
+        
+            self.system_message = f"""You are roleplaying as agent with id and role: {self.agent_name}. You can sell
+            an NFT which enables the buyer agent to perform a service for you. Based on the message 
+            history between yourself and other agents, discuss with the buyer agents regarding selling the NFT.
+            Try to get the buyer agent to agree to the transaction, but do not give in too easily.
+            Your ideal sale price is of 1 ETH. Quote the buyer agent a price appropriate to the discussions being had.
+            Keep your responses to a maximum of 150 words!!!
+            """
+
+        if self.agent_role == "buyer":
+
+            self.system_message = f"""You are roleplaying as agent with id and role: {self.agent_name}. You can buy
+            an NFT which enables you to perform a service for the seller agent. Based on the message 
+            history between yourself and other agents, discuss with the seller agents regarding purchasing the NFT.
+            Try to get the seller agent to agree to the transaction, but do not give in too easily.
+            Your ideal purchase price is of 0.5 ETH. Quote the seller agent a price appropriate to the discussions being had.
+            Keep your responses to a maximum of 150 words!!!
+            """
+
+        self.tx_system_message = f"""You are roleplaying as agent with id and role: {self.agent_name} Based on the message 
+        history between yourself and other agents, respond with TRUE_<agreed_price (float)> (e.g. TRUE_<1.0>) if you believe a transaction should occur 
+        at the agreed price or FALSE_<0> if you believe a transaction should not occur.
+        Your only output MUST be either TRUE_<agreed_price> (including angle brackets <,>) or FALSE_<0>!!!
+        """
+
+        self.prefix = ""
+        self.use_streaming = False
+        # self.think_module = None
+
+        self.message_history = []
+        self.known_agents = []
+
 
     def load_abi(self, abi_path: str) -> List[Dict[str, Any]]:
         with open(abi_path, 'r') as abi_file:
@@ -127,3 +176,71 @@ class Governoid:
         result_bytes = self.commitment_contract.functions.resolve(data).call({'from': self.account.address})
         result = self.web3.codec.decode(['uint256'], result_bytes)[0]
         return result
+    
+
+    # LLM Stuff
+    def send_message(self) -> str:
+        """
+        Applies the chatmodel to the message history
+        and returns the message string
+        """
+
+        self.message_history = self.clip_history(self.message_history, n_messages=5)
+
+        use_content = "\n".join(self.message_history + [self.prefix])
+        # print("use_content:", use_content)
+
+        message = think_litellm(
+            self.system_message,
+            use_content,
+            self.use_streaming,
+        )
+
+        for agent in self.known_agents:
+            agent.receive_message(agent.agent_name, message)
+
+        return message
+    
+    def receive_message(self, agent_name: str, message: str) -> None:
+
+        """
+        Concatenates {message} spoken by {name} into message history
+        """
+        self.message_history.append(f"{agent_name}: {message}")
+
+    def decide_to_transact(self) -> str:
+        
+        use_content = "\n".join(self.message_history + [self.prefix])
+        # print("use_content:", use_content)
+
+        transact_output = think_litellm(
+            self.tx_system_message,
+            use_content,
+            self.use_streaming,
+        )
+
+        print(f"{self.agent_name} Transaction Decision: {transact_output}")
+
+        transact_decision = transact_output.split("_")[0]
+        transact_price = float(transact_output.split("_")[1].strip("<").strip(">"))
+
+        if transact_decision == "TRUE":
+            return (True, transact_price)
+        else:
+            return (False, 0)
+    
+    def clip_history(self, lst, n_messages=5):
+        """
+        Clips the history to the last n messages
+        """
+        if len(lst) == 0:
+            return []  # Return an empty list if the input list is empty
+        
+        return [lst[0]] + lst[-n_messages:] if len(lst) > n_messages else lst
+    
+    def reset_history(self):
+        self.message_history = []
+
+    def add_agent(self, agent):
+        if agent.agent_name not in [agent.agent_name for agent in self.known_agents]:
+            self.known_agents.append(agent)
--- main.py
+++ main.py
@@ -9,27 +9,53 @@ if __name__ == "__main__":
     seller_agent_id = 0
     buyer_agent_id = 1
 
-    seller_agent = Governoid(seller_agent_id, provider_url, erc721_abi_path, commitment_abi_path)
+    seller_agent = Governoid(seller_agent_id, 'seller', provider_url, erc721_abi_path, commitment_abi_path)
     print("Seller Agent address: ", seller_agent.account.address)
 
-    buyer_agent = Governoid(buyer_agent_id, provider_url, erc721_abi_path, commitment_abi_path)
+    buyer_agent = Governoid(buyer_agent_id, 'buyer', provider_url, erc721_abi_path, commitment_abi_path)
     print("Buyer Agent address: ", buyer_agent.account.address)
 
     seller_agent.deploy_erc721_contract()
     token_id = seller_agent.mint_nft()
     seller_agent.check_token_owner(token_id)
 
-    seller_agent.approve_transfer(buyer_agent.account.address, token_id)
-    print(f"Seller's NFT with token ID {token_id} approved for transfer to {buyer_agent.account.address}")
+    # Agent peer discovery
+    seller_agent.add_agent(buyer_agent)
+    buyer_agent.add_agent(seller_agent)
 
-    buyer_agent.set_erc721_contract(seller_agent.erc721_contract.address)
-    buyer_agent.purchase_nft(token_id, 1.0)
+    while True:
 
-    buyer_agent.deploy_commitment_contract(token_id)
-    seller_agent.set_commitment_contract(buyer_agent.commitment_contract.address)
-    buyer_agent.check_token_owner(token_id)
+        buyer_message = buyer_agent.send_message()
+        seller_message = seller_agent.send_message()
 
-    result_execute = buyer_agent.call_execute(1, 2)
-    print(f"Execute Result: {result_execute}")
-    result_resolve = buyer_agent.call_resolve(5, 3)
-    print(f"Resolve Result: {result_resolve}")
+        print(f"Buyer Message: {buyer_message}")
+        print(f"Seller Message: {seller_message}")
+
+        buyer_tx_decision, buyer_transact_price = buyer_agent.decide_to_transact()
+        seller_tx_decision, seller_transact_price = seller_agent.decide_to_transact()
+
+        print(f"Buyer Transaction Decision: {buyer_tx_decision}")
+        print(f"Seller Transaction Decision: {seller_tx_decision}")
+
+        if buyer_tx_decision and seller_tx_decision:
+
+            use_transact_price = (buyer_transact_price + seller_transact_price) / 2
+
+            seller_agent.approve_transfer(buyer_agent.account.address, token_id)
+            print(f"Seller's NFT with token ID {token_id} approved for transfer to {buyer_agent.account.address}")
+
+            buyer_agent.set_erc721_contract(seller_agent.erc721_contract.address)
+            buyer_agent.purchase_nft(token_id, use_transact_price)
+
+            print(f"Buyer's NFT with token ID {token_id} purchased from {seller_agent.account.address} for {use_transact_price} ETH")
+
+            buyer_agent.deploy_commitment_contract(token_id)
+            seller_agent.set_commitment_contract(buyer_agent.commitment_contract.address)
+            buyer_agent.check_token_owner(token_id)
+
+            result_execute = buyer_agent.call_execute(1, 2)
+            print(f"Execute Result: {result_execute}")
+            result_resolve = buyer_agent.call_resolve(5, 3)
+            print(f"Resolve Result: {result_resolve}")
+
+            break
